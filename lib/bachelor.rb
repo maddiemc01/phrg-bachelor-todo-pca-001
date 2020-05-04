0caf610cf9ffbe384ea4e5daa40e18461f608fc8
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-04 10:48:09 -0400

    passes first 9 tests

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,29 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |key|
+    return key["name"].split(" ").first if key["status"] == "Winner"
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |key, value|
+    value.each do |key1|
+      return key1["name"] if key1["occupation"] == occupation
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |_key, value|
+    value.each do |key1|
+      if key1["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
